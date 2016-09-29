8018e6e4bf97b9ba3ccdfde76532eeb168cc5915
slindquist3 <slindquist3@gmail.com>
2016-09-29 14:10:56 -0400

    Practiced more with constants

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(student_name, grade)
+
+      @roster[grade] ||= []
+      @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grades, student_lists|
+      student_lists.sort!
+    end
+    @roster
+  end
+
+end
